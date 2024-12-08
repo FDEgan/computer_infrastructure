23c359083826a12d56dcd6d5c9b603d667ac74e6
FDEgan <157654218+FDEgan@users.noreply.github.com>
2024-12-08 19:15:38 +0000

    Updating weather scripts.

--- weather.sh
+++ weather.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
+date
+echo "Downloading weather data"
 wget -O `date +"%Y%m%d_%H%M%S"`.json https://prodapi.metweb.ie/observations/athenry/today
+echo "Weather data downloaded"
+date
