69ef46e6290b5822adfbb93855442dc6c6b2ec8e
FDEgan <157654218+FDEgan@users.noreply.github.com>
2024-12-19 11:37:42 +0000

    Updating script to add files to folder.

--- .github/weather.sh
+++ .github/weather.sh
@@ -2,6 +2,6 @@
 mkdir -p weather_files
 date
 echo "Downloading weather data"
-wget -O `date +"%Y%m%d_%H%M%S"`.json https://prodapi.metweb.ie/observations/athenry/today
+wget -O weather_files/`date +"%Y%m%d_%H%M%S"`.json https://prodapi.metweb.ie/observations/athenry/today
 echo "Weather data downloaded"
 date
